ca4ecde1783fd62f70c865ead8001409c76a3237
aniqdev <aniq.dev@gmail.com>
2020-11-11 15:02:38 +0200

    first commit-3

--- lesson-4-gallery/index.php
+++ lesson-4-gallery/index.php
@@ -32,22 +32,50 @@
 
 <pre>
 <?php
-	$dir = scandir('images');
-	// print_r($dir);
 	// for ($i=2; $i < count($dir); $i++) {
 	// 	echo $dir[$i];
 	// 	echo "<hr>";
 	// }
-	print_r($_POST);
+	// print_r($_POST);
 	if (isset($_POST['delete'])) {
 		unlink('images/'.$_POST['delete']);
 	}
+	$dir = scandir('images');
+	// print_r($dir);
 ?>
 </pre>
 	<br>
-	<div class="gallery" style="display: flex; flex-wrap: wrap; justify-content: space-between;">
+	<div class="">
+<div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-ride="carousel">
+  <ol class="carousel-indicators">
+  	<?php for ($i=2; $i < count($dir); $i++): ?>
+    <li data-target="#carouselExampleCaptions" data-slide-to="<?= $i-2 ?>" class="<?= $i===2 ? 'active' : '' ?>"></li>
+	<?php endfor; ?>
+  </ol>
+  <div class="carousel-inner">
+  	<?php for ($i=2; $i < count($dir); $i++): ?>
+    <div class="carousel-item <?= $i===2 ? 'active' : '' ?>">
+      <img src="images/<?php echo $dir[$i] ?>" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5 style="background: rgb(0 0 0 / 45%); padding: 15px;"><?php echo $dir[$i] ?></h5>
+      </div>
+    </div>
+	<?php endfor; ?>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+	</div><!-- .carousel -->
+	<br>
+	<div class="gallery" style="display: flex; flex-wrap: wrap;">
 		<?php for ($i=2; $i < count($dir); $i++){ ?>
-		<div class="card" style="width: 18rem; margin-bottom: 15px;" >
+		<div class="card" style="width: 18rem; margin: 0 15px 15px 0" >
 		  <img src="images/<?php echo $dir[$i] ?>" class="card-img-top" alt="...">
 		  <div class="card-body">
 		    <p class="card-text"><?php echo $dir[$i] ?></p>
