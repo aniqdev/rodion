115db199e6a035a69c242b3bd2d1b726b0f875bb
aniqdev <aniq.dev@gmail.com>
2020-11-25 22:45:49 +0200

    users crud

--- lesson-db/header.php
+++ lesson-db/header.php
@@ -3,6 +3,8 @@
   <head>
     <title>Uebung5(1)_tabelle.php</title>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <style>
 .navbar-light .navbar-nav .active>.nav-link, .navbar-light .navbar-nav .nav-link.active{
--- lesson-db/index.php
+++ lesson-db/index.php
@@ -13,11 +13,11 @@
 
 // print_r($result);
 
-print_r($_POST);
+// print_r($_POST);
 
 $post_data = db_escape($_POST);
 
-if (isset($_POST['add-user'])) {
+if (isset($_POST['add-user']) && $_POST['add-user'] === '') {
 	$position = $post_data['position'];
 	$email = $post_data['email'];
 	$name = $post_data['name'];
@@ -38,16 +38,49 @@ if (isset($_POST['add-user'])) {
 		`password` = '$password',
 		address = '$address',
 		city = '$city',
-		`index` = '$index'";
-
-	print_r($query);
+		`index` = '$index' ";
 
 	db_query($query);
 }
 
-	// $hash = md5('#$%^&*('.'123');
-	// var_dump($hash === '70421e2d2babe37e201a887d878440a6');
-	// print_r($hash);
+// print_r($_GET);
+// var_dump(isset($_GET['user_id']));
+
+if (isset($_POST['add-user']) && $_POST['add-user'] !== '') {
+	$user_id = (int)$_POST['add-user'];
+
+	$position = $post_data['position'];
+	$email = $post_data['email'];
+	$name = $post_data['name'];
+	$last_name = $post_data['last_name'];
+	$username = $post_data['username'];
+	$password = $post_data['password'];
+	$password_confirmation = $post_data['password_confirmation'];
+	$address = $post_data['address'];
+	$city = $post_data['city'];
+	$index = $post_data['index'];
+
+	db_query("UPDATE users SET
+		`position` = '$position',
+		email = '$email',
+		name = '$name',
+		last_name = '$last_name',
+		username = '$username',
+		`password` = '$password',
+		address = '$address',
+		city = '$city',
+		`index` = '$index'
+		WHERE id = '$user_id' ");
+}
+
+if (isset($_GET['user_id'])) {
+	$user_id = (int)$_GET['user_id'];
+
+	$user = db_query("SELECT * FROM users WHERE id = '$user_id' ");
+	// print_r($user);
+	if($user) $user = $user[0];
+	// print_r($user);
+}
 ?>
 </pre>
 
@@ -58,55 +91,55 @@ if (isset($_POST['add-user'])) {
   <div class="form-row">
 	<div class="form-group col-md-6">
 	  <label for="inputEmail4">Position</label>
-	  <input name="position" type="text" class="form-control" id="inputEmail4">
+	  <input name="position" value="<?= isset($user['position']) ? $user['position'] : '' ?>" type="text" class="form-control" id="inputEmail4">
 	</div>
 	<div class="form-group col-md-6">
 	  <label for="inputPassword4">email</label>
-	  <input name="email" type="email" class="form-control" id="inputPassword4">
+	  <input value="<?= isset($user['email']) ? $user['email'] : '' ?>" name="email" type="email" class="form-control" id="inputPassword4">
 	</div>
   </div>
 
   <div class="form-row">
 	<div class="form-group col-md-6">
 	  <label for="inputPassword4">Name</label>
-	  <input name="name" type="text" class="form-control" id="inputPassword4">
+	  <input value="<?= isset($user['name']) ? $user['name'] : '' ?>" name="name" type="text" class="form-control" id="inputPassword4">
 	</div>
 	<div class="form-group col-md-6">
 	  <label for="inputEmail4">Last name</label>
-	  <input name="last_name" type="text" class="form-control" id="inputEmail4">
+	  <input value="<?= isset($user['last_name']) ? $user['last_name'] : '' ?>" name="last_name" type="text" class="form-control" id="inputEmail4">
 	</div>
   </div>
 
   <div class="form-row">
 	<div class="form-group col-md-4">
 	  <label for="inputPassword4">Username</label>
-	  <input name="username" type="text" class="form-control" id="inputPassword4">
+	  <input value="<?= isset($user['username']) ? $user['username'] : '' ?>" name="username" type="text" class="form-control" id="inputPassword4">
 	</div>
 	<div class="form-group col-md-4">
 	  <label for="inputEmail4">Password</label>
-	  <input name="password" type="password" class="form-control" id="inputEmail4">
+	  <input value="<?= isset($user['password']) ? $user['password'] : '' ?>" name="password" type="password" class="form-control" id="inputEmail4">
 	</div>
 	<div class="form-group col-md-4">
 	  <label for="inputEmail4">Confirm password</label>
-	  <input name="password_confirmation" type="password" class="form-control" id="inputEmail4">
+	  <input value="<?= isset($user['password']) ? $user['password'] : '' ?>" name="password_confirmation" type="password" class="form-control" id="inputEmail4">
 	</div>
   </div>
 
   <div class="form-group">
 	<label for="inputAddress">Address</label>
-	<input name="address" type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+	<input value="<?= isset($user['address']) ? $user['address'] : '' ?>" name="address" type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
   </div>
   <div class="form-row">
 	<div class="form-group col-md-6">
 	  <label for="inputCity">City</label>
-	  <input name="city" type="text" class="form-control" id="inputCity">
+	  <input value="<?= isset($user['city']) ? $user['city'] : '' ?>" name="city" type="text" class="form-control" id="inputCity">
 	</div>
 	<div class="form-group col-md-2">
 	  <label for="inputZip">Zip</label>
-	  <input name="index" type="text" class="form-control" id="inputZip">
+	  <input value="<?= isset($user['index']) ? $user['index'] : '' ?>" name="index" type="text" class="form-control" id="inputZip">
 	</div>
   </div>
-  <button name="add-user" type="submit" class="btn btn-primary">Sign in</button>
+  <button name="add-user" value="<?= isset($user['id']) ? $user['id'] : '' ?>" type="submit"  class="btn btn-primary">Sign in</button>
 </form>
 </div>
 
