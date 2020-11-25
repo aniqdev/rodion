4911ef77a40cfe5c77af15374a5eacbed55a8432
aniqdev <aniq.dev@gmail.com>
2020-11-25 21:08:16 +0200

    inser into dbs

--- lesson-db/functions.php
+++ lesson-db/functions.php
@@ -9,6 +9,11 @@ define('db_NAME', 'test_db');
 
 function db_query($query = '')
 {
-
+	if(!$query) return [];
+	if (stripos($query, 'select') === 0 || stripos($query, 'show') === 0 || stripos($query, 'describe') === 0) {
+		return DB::getInstance()->get_results($query);
+	}else{
+		return DB::getInstance()->query($query);
+	}
 }
 
